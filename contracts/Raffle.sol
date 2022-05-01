5f93aebee667e8ab8de7743ada72487d11d34253
Chema De la Cruz Sanchez <chemadelacruzsanchez@MBP-de-Chema-2.fritz.box>
2022-05-01 18:48:51 +0200

    contract update

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,10 +2,27 @@
 
 pragma solidity ^0.8.7;
 
+error Raffle__SendMoreToEnterRaffle();
+
 contract Raffle {
 
+    uint256 public immutable i_entranceFee; // Fee to join the Raffle (Immutable: unchangeable and cheaper)
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    /**
+    * @notice Function for users to join the Raffle
+    * @dev
+    */
+    function enterRaffle() external payable {
+        // require(msg.value > i_entranceFee, "Not enough money sent."); Better custom error
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+    }
 
 
 
-    
 }
