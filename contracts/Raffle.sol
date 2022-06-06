e6f5db14b6fe0b5ba8340fcfc5f2d7374a9cb98e
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-06 18:03:53 +0200

    update

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,13 +4,19 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol"; // to work with COORDINATOR and VRF
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol"; // to use functionalities for Chainlink VRF
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol"; // use Chainlink Keepers
 
 error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
 error Raffle__UpkeepNotNeeded();
 error Raffle__TransferFailed();
 
-contract Raffle is VRFConsumerBaseV2 {
+/**@title A Raffle Smart Contract
+ * @author JMariadlcs
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
+ */
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     
     /// @notice Enums
     // To let user enter the Raffle or not
@@ -20,20 +26,22 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     /// @notice State Variables
-    RaffleState public s_raffleState; // s_ because 'store' and we are going to change the value
     uint256 private i_entranceFee; // Fee to join the Raffle (Immutable: unchangeable and cheaper)
-    uint256 public immutable i_interval; // interval when Raffle State should change
-    uint256 public s_lastTimeStamp; // to get last time Raffle State changed
+    uint256 private immutable i_interval; // interval when Raffle State should change
     address payable[] private s_players; // to have raffle players
-    address public s_recentWinner; // most recent Winner
+
+     /// @notice Lottery Variables
+    address private s_recentWinner; // most recent Winner
+    RaffleState private s_raffleState; // s_ because 'store' and we are going to change the value
+    uint256 private s_lastTimeStamp; // to get last time Raffle State changed
 
     /// @notice VRF Variables
-    VRFCoordinatorV2Interface public immutable i_vrfCoordinator; // coordinator for working with Chainlink VRF 
-    bytes32 public immutable i_gasLane;
-    uint64 public immutable i_subscriptionId;
-    uint32 public immutable i_callbackGasLimit;
-    uint16 public constant REQUEST_CONFIRMATIONS = 3; // how many blocks are needed to be considered complete
-    uint32 public constant NUM_WORDS = 1; // how many random numbers we want to get
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator; // coordinator for working with Chainlink VRF 
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3; // how many blocks are needed to be considered complete
+    uint32 private constant NUM_WORDS = 1; // how many random numbers we want to get
 
     /// @notice Events
     event RaffleEnter(address indexed player); // when a user enters Raffle
@@ -78,10 +86,15 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     /**
-    * @notice Functions for select a RANDOMand AUTOMATICALLY WINNER -> Chainlink Keepers
+    * @notice Functions for select a RANDOMand AUTOMATICALLY WINNER -> Chainlink Keepers || DIRECTLY DONE BY KEEPERS BELOW
     * @dev 
     * 1 - Want it done Automatically
     * 2 - Want it to be really RANDOM
+    function requestRandomWinner() external {
+        s_raffleState = RaffleState.Calculating;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS);
+        emit RequestedRaffleWinner(requestId);
+    }
     */
 
     /**
@@ -93,20 +106,20 @@ contract Raffle is VRFConsumerBaseV2 {
     * 4 - There is al least 1 player
     * 5- Keepers has LINK
     */
-    function checkUpkeep(bytes memory /* checkData */) public view returns(bool upkeedNeeded, bytes memory /*performData*/) {
+    function checkUpkeep(bytes memory /* checkData */) public view override returns(bool upkeedNeeded, bytes memory /*performData*/) {
         bool isOpen = RaffleState.Open == s_raffleState; // 2 - check if its open
         bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval); // 1 - true if timePassed is > interval
         bool hasBalance = address(this).balance > 0; // 3 - check if contract has ETH
         bool hasPlayers = s_players.length > 0; // 4 - check if there is al least 1 player
 
-        upkeedNeeded = (timePassed && isOpen && hasBalance && hasPlayers); // EVERY REQUIREMENT IS FULFILLED
+        upkeedNeeded = (timePassed && isOpen && hasBalance && hasPlayers); // EVERY REQUIREMENT IS FULFILLED -> true
         return (upkeedNeeded,"0x0");
     }
 
     /**
     * @notice Functions to actually pick a random NUMBER -> Chainlink keepers to do it Automatically and decentralized
     */
-    function performUpkeep(bytes calldata /* performData */) external {
+    function performUpkeep(bytes calldata /* performData */) external override {
         (bool upkeepNeeded, ) = checkUpkeep(""); //checkUpkeep returns 2 parameters but we only need first 1
         if (!upkeepNeeded) { // requirements not met
             revert Raffle__UpkeepNotNeeded();
@@ -150,5 +163,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns(address) {
         return s_players[index];
+    }
+
+    function getRecentWinnter() public view returns(address) {
+        return s_recentWinner;
     }
 }
