fb7f6ec7a0b924bb9d066e680fb286a9ef11449c
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-12 19:41:43 +0200

    updates

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -4,7 +4,7 @@
 
 const { frontEndContractsFile, frontEndAbiFile } = require("../helper-hardhat-config")
 const fs = require("fs")
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 
 module.exports = async () => {
     if (process.env.UPDATE_FRONT_END) {
@@ -24,12 +24,16 @@ async function updateContractAddresses() {
     const raffle = await ethers.getContract("Raffle")
     const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
     if (network.config.chainId.toString() in contractAddresses) {
+        console.log("Writing contract")
         if (!contractAddresses[network.config.chainId.toString()].includes(raffle.address)) {
+            console.log("Writing contract2")
             contractAddresses[network.config.chainId.toString()].push(raffle.address)
         }
     } else {
+        console.log("Writing contract3")
         contractAddresses[network.config.chainId.toString()] = [raffle.address]
     }
+    console.log(frontEndContractsFile)
     fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
 }
 module.exports.tags = ["all", "frontend"]
